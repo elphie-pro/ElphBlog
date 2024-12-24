0c72d67c23a07c97a915a6a45fcb214c8df5c26a
elphie-pro <onazjr2006@gmail.com>
2024-12-24 20:16:46 +0100

    'env'

--- src/firebase-config.js
+++ src/firebase-config.js
@@ -13,6 +13,8 @@ const firebaseConfig = {
   appId: import.meta.env.VITE_FIREBASE_APP_ID,
   measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID,
 };
+console.log("Firebase API Key:", import.meta.env.VITE_API_KEY);
+
 
 
 const app = initializeApp(firebaseConfig);
